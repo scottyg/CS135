42f7eea8b0f1cbafd727d55f4a39d8b09404bfb3
Scott Gordon <me@scottyg.net>
2022-02-07 19:13:37 -0800

    lab02b and pa02b

---
+++ lab02b/lab02b.cpp
@@ -0,0 +1,62 @@
+/// @file lab02b.cpp
+/// @author Scott Gordon
+/// @date 2-7-22
+/// @note I pledge my word of honor that I have complied with the
+/// CSN Academic Integrity Policy while completing this assignment.
+/// @brief Ask for days, hours, minutes and seconds. Convert to seconds.
+/// Print output to screen.
+/// @note 15 minutes were taken to develop, write, test and debug solution.
+
+// include statements
+#include <iostream>
+#include <string>
+
+// using namespace statement
+using namespace std;
+
+int main() {
+    // constant definitions
+    const int SECRET = 11;      // SECRET number
+    const double RATE = 12.50;  // Not Used
+
+    // variable declarations
+    int num1;               // Number 1
+    int num2;               // Number 2
+    int new_num;            // Numbers added together and eventually add SECRET
+    string name;            // Users last name
+    double hours_worked;    // Not Used
+    double wages;           // Not Used
+
+    // input phase
+    cout << "Enter num1: ";
+    cin >> num1;
+    cout << "Enter num2: ";
+    cin >> num2;
+    cout << "Enter your last name: ";
+    cin >> name;
+
+    // processing phase / output phase
+    cout << "The value of num1 = "
+         << num1
+         << ", and the value of num2 = "
+         << num2
+         << "."
+         << endl;
+    new_num = num1 + num2;
+    cout << "The value of new_num is "
+         << new_num
+         << "."
+         << endl;
+    new_num = new_num + SECRET;
+    cout << "The new value of new_num is "
+         << new_num
+         << "."
+         << endl;
+    cout << "Your last name is "
+         << name
+         << "."
+         << endl;
+
+    // return statement
+    return 0;
+}
---
+++ pa02b/pa02b.cpp
@@ -0,0 +1,60 @@
+/// @file pa02b.cpp
+/// @author Scott Gordon
+/// @date 2-7-22
+/// @note I pledge my word of honor that I have complied with the
+/// CSN Academic Integrity Policy while completing this assignment.
+/// @brief Ask for days, hours, minutes and seconds. Convert to seconds.
+/// Print output to screen.
+/// @note 30 minutes were taken to develop, write, test and debug solution.
+
+// include statements
+#include <iostream>
+
+// using namespace statement
+using namespace std;
+
+int main() {
+    // constant definitions
+    const int SECONDS_TO_MINUTES = 60;
+    const int MINUTES_TO_HOURS = 60;
+    const int HOURS_TO_DAYS = 24;
+
+    // variable declarations
+    long seconds;       // Time in seconds
+    int tz_offset;      // Timezone offset
+    int days;           // Time converted to days
+    int hours;          // Time converted to hours
+    int minutes;        // Time converted to minutes
+
+    // input phase
+    cout << "Enter time(seconds) and TZ offset(hours): ";
+    cin >> seconds >> tz_offset;
+    cout << endl;
+
+    // processing phase
+    days = seconds / SECONDS_TO_MINUTES / MINUTES_TO_HOURS / HOURS_TO_DAYS;
+    seconds = seconds - (days * HOURS_TO_DAYS * MINUTES_TO_HOURS * SECONDS_TO_MINUTES);
+    hours = seconds / SECONDS_TO_MINUTES / MINUTES_TO_HOURS;
+    seconds = seconds - (hours * MINUTES_TO_HOURS * SECONDS_TO_MINUTES);
+    hours = hours + tz_offset;
+    if(hours > 24){
+        days++;
+        hours = hours - 24;
+    }
+    minutes = seconds / SECONDS_TO_MINUTES;
+    seconds = seconds - (minutes * SECONDS_TO_MINUTES);
+
+    //output phase
+    cout << days
+         << "d "
+         << hours
+         << "h "
+         << minutes
+         << "m "
+         << seconds
+         << "s "
+         << endl;
+
+    // return statement
+    return 0;
+}
