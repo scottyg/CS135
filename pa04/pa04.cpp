d76cf69b5fb3d20d6051461e1bb6e85c5290918e
Scott Gordon <me@scottyg.net>
2022-02-21 14:26:46 -0800

    Start 04 assignments

---
+++ pa04/pa04.cpp
@@ -0,0 +1,28 @@
+/// @file pa04.cpp
+/// @author Scott Gordon
+/// @date 2-21-22
+/// @note I pledge my word of honor that I have complied with the
+/// CSN Academic Integrity Policy while completing this assignment.
+/// @brief 
+/// @note x minutes were taken to develop, write, test and debug solution.
+
+// include statements
+#include <iostream>
+
+// using namespace statement
+using namespace std;
+
+int main() {
+    // constant definitions
+    
+    // variable declarations
+    
+    // input phase
+    
+    // processing phase
+    
+    // output phase
+
+    // return statement
+    return 0;
+}
