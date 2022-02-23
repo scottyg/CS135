e254d87a8c96febfadabb88339298367b67c9112
Scott Gordon <me@scottyg.net>
2022-02-23 06:15:02 -0800

    Lab04

--- lab04/lab04.cpp
+++ lab04/lab04.cpp
@@ -3,26 +3,48 @@
 /// @date 2-21-22
 /// @note I pledge my word of honor that I have complied with the
 /// CSN Academic Integrity Policy while completing this assignment.
-/// @brief 
-/// @note x minutes were taken to develop, write, test and debug solution.
+/// @brief Determine if a rectangle or square has the larger area and if one...
+/// @brief can fit inside the other.
+/// @note 30 minutes were taken to develop, write, test and debug solution.
 
-// include statements
 #include <iostream>
 
-// using namespace statement
 using namespace std;
 
-int main() {
-    // constant definitions
-    
-    // variable declarations
-    
-    // input phase
-    
-    // processing phase
-    
-    // output phase
+/// ---------------------------------------------------------------------------
+/// Main entry-point for this application.
+///
+/// @returns Exit-code for the process - 0 for success
+/// ---------------------------------------------------------------------------
 
-    // return statement
+int main() {
+    // local variable declaration(s)
+    double width;       // Width of rectangle
+    double height;      // Height of rectangle
+    double square_side; // Side of a square
+
+    // input phase
+    cout << "Enter width, height, square_side: ";
+    cin >> width >> height >> square_side;
+
+    // processing phase and output phase
+    cout << endl;
+    // Check which object has the larger area
+    if ((width * height) < (square_side * square_side)) {
+        cout << "The object with the greatest area is the square." << endl;
+    } else if (width * height == square_side * square_side) {
+        cout << "The square and the rectangle have the same area." << endl;
+    } else {
+        cout << "The object with the greatest area is the rectangle." << endl;
+    }
+    // Check which object fits inside the other
+    if (width > square_side && height > square_side) {
+        cout << "The square fits inside the rectangle." << endl;
+    } else if (width < square_side && height < square_side) {
+        cout << "The rectangle fits inside the square." << endl;
+    } else {
+        cout << "Neither shape fits inside the other." << endl;
+    }
+
     return 0;
 }
